489e75aea9c93d9110be95020fdb733fe4b2f27a
vandat <trieudat2211@gmail.com>
2025-02-27 22:03:03 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Van Dat on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
